8f1d84f7ef0ea6c7b22cf8a0058484b31ceb5391
pabloturati <pablo.turati@gmail.com>
2018-07-17 09:15:43 -0500

    cambios en el git para ver env

--- routes/items.js
+++ routes/items.js
@@ -30,29 +30,6 @@ router.post('/newItem', uploadCloud.single('photoURL'), (req,res, next) => {
     .catch(e=>console.log(e))
 })
 
-
-// router.post('/signup', (req,res,next)=>{
-//     if(req.body.password !== req.body.password2){
-//         req.body.err = "Tu password no coincide"
-//         res.render('auth/signup', req.body)
-//     }
-//     User.register(req.body, req.body.password)
-//     .then(user=>{
-//         console.log('entraste')
-//         //activation link
-//         sendActivationLink(user);
-//         //loguearlo automaticamente
-//         res.redirect('/login')
-//     })
-//     .catch(e=>{
-//         req.body.err = errDict[e.name];
-//         res.render('auth/signup', req.body)
-//     });
-// });
-
-
-
-
 //Entrar al detalle de un item
 router.get('/:id', (req,res) =>{
     res.send("hola detalle items")
